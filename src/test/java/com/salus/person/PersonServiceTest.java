360844211bf4ad964e3b96f0a1c6b2b1f3be7489
Mayndi Borges <mayndi15@gmail.com>
2023-08-09 00:35:07 -0300

    Add tests service method delete

--- src/test/java/com/salus/person/PersonServiceTest.java
+++ src/test/java/com/salus/person/PersonServiceTest.java
@@ -5,6 +5,7 @@ import com.salus.person.validators.PersonServiceValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -12,6 +13,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class PersonServiceTest {
@@ -31,39 +36,74 @@ class PersonServiceTest {
     }
 
     @Test
+    void whenDeleteNullIdThenReturnAnNull() throws SalusException {
+        when(repository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(null));
+
+        Long id = null;
+        service.delete(id);
+
+        verify(repository, Mockito.times(1)).delete(null);
+        verify(validator, Mockito.times(1)).validateId(id);
+    }
+
+    @Test
+    void whenDeleteIdNotExistsThenReturnAnNull() throws SalusException {
+        when(repository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(null));
+
+        Long id = 1L;
+        service.delete(id);
+
+        verify(repository, Mockito.times(1)).delete(null);
+        verify(validator, Mockito.times(1)).validateId(id);
+    }
+
+    @Test
+    void whenDeleteExistsIdThenReturnAnPerson() throws SalusException {
+        Person person = new Person();
+        when(repository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(person));
+        doNothing().when(repository).delete(person);
+
+        Long id = 1L;
+        service.delete(id);
+
+        verify(repository, Mockito.times(1)).delete(person);
+        verify(validator, Mockito.times(1)).validateId(id);
+    }
+
+    @Test
     void whenDetailsNullIdThenReturnAnNull() throws SalusException {
         Long id = null;
-
         var result = service.details(id);
 
-        Mockito.verify(repository, Mockito.times(0)).findById(id);
-        Mockito.verify(validator, Mockito.times(1)).validateId(id);
+        verify(repository, Mockito.times(0)).findById(id);
+        verify(validator, Mockito.times(1)).validateId(id);
 
         Assertions.assertNull(result);
     }
 
     @Test
     void whenDetailsIdNotExistsThenReturnAnNull() throws SalusException {
-        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(null));
+        when(repository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(null));
 
         Long id = 1L;
         var result = service.details(id);
 
-        Mockito.verify(repository, Mockito.times(1)).findById(id);
-        Mockito.verify(validator, Mockito.times(1)).validateId(id);
+        verify(repository, Mockito.times(1)).findById(id);
+        verify(validator, Mockito.times(1)).validateId(id);
 
         Assertions.assertNull(result);
     }
 
     @Test
     void whenDetailsExistsIdThenReturnAnPerson() throws SalusException {
-        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Person()));
+        Person person = new Person();
+        when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(person));
 
         Long id = 1L;
         var result = service.details(id);
 
-        Mockito.verify(repository, Mockito.times(1)).findById(id);
-        Mockito.verify(validator, Mockito.times(1)).validateId(id);
+        verify(repository, Mockito.times(1)).findById(id);
+        verify(validator, Mockito.times(1)).validateId(id);
 
         Assertions.assertNotNull(result);
         Assertions.assertEquals(result.getClass(), Person.class);
